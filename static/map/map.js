99e738b6f359e39b7e448fa6ab3480be58bf9497
Jim Ryan <seamus0g@hotmail.com>
2016-01-27 15:37:03 -0800

    Added test mapbox.com projectId and accessToken. TODO: create a project mapbox account.

--- static/map/map.js
+++ static/map/map.js
@@ -7,4 +7,12 @@ function createMap() {
         center: [38.551253, -121.488683],
         zoom: 13
     });
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'jimryan.p0f37ng8',
+        accessToken: 'pk.eyJ1IjoiamltcnlhbiIsImEiOiJjaWp4Z2NmZ3QweTk1dmdsejM0NTk3cXZnIn0.BEc764xfxFGOr8HBQvmN7g'
+    }).addTo(map);
 }
